be41229c899beda7d1ac2d576b832b304f165fba
rharris23 <59853289+rharris23@users.noreply.github.com>
2020-01-30 20:28:31 -0500

    Update Lab2.java

--- Lab2.java
+++ Lab2.java
@@ -1,26 +1,68 @@
-public class Lab2
+public class DrawingLab
 {
-  public static void cleanSquare()
-  {
-    Robot.load("square.txt");
-    Robot.setDelay(0.1);
+    public static void exercise1()
+    {
+        //insert code here to make a new window with your first drawing
+        Draw.window(500,500);
+        Draw.setFill(true);
+        Draw.setColor(255,0,0);
+        Draw.oval(175,175,150,150);
+        Draw.setColor(255,255,255);
+        Draw.oval(200,200,100,100);
+        Draw.setColor(255,0,0);
+        Draw.oval(225,225,50,50);
+        
+
+    }
+
+    public static void exercise2()
+    {
+        //insert code here to make a new window with your second drawing
+        Draw.window(200,600);
+        Draw.setFill(true);
+        Draw.setColor(255,0,0);
+        Draw.oval(0,0,200,200);
+        Draw.setColor(255,255,0);
+        Draw.oval(0,200,200,200);
+        Draw.setColor(0,255,0);
+        Draw.oval(0,400,200,200);
+
+    }
+
+    public static void exercise3()
+    {
+        //insert code here to make a new window with your third drawing
+        Draw.window(400,400);
+        Draw.setFill(true);
+        Draw.setColor(0,0,255);
+        Draw.rectangle(0,0,800,800);
+        Draw.setColor(255,0,0);
+        Draw.rectangle(50,50,100,100);
+        Draw.rectangle(250,250,100,100);
+        Draw.rectangle(200,0,200,200);
+        Draw.rectangle(0,200,200,200);
+        Draw.setColor(0,0,255);
+        Draw.rectangle(250,50,100,100);
+        Draw.rectangle(50,250,100,100);
+
+    }
+
+    public static void exercise4()
+    {
+        //insert code here to make a new window with your fourth drawing
+        Draw.window(800,800);
+        Draw.setFill(true);
+        Draw.setColor(0,0,255);
+        Draw.polygon(100,400,200,100,600,100,700,400,400,600);
+
+    }
     
-    //INSERT CODE HERE
-  }
-  
-  public static void darkenComb()
-  {
-    Robot.load("comb.txt");
-    Robot.setDelay(0.05);
-    
-    //INSERT CODE HERE
-  }
-  
-  public static void makeCheckered()
-  {
-    Robot.load("blank.txt");
-    Robot.setDelay(0.05);
-    
-    //INSERT CODE HERE
-  }
-}
+     public static void exercise5()
+    {
+        //insert code here to make a new window with your fourth drawing
+        Draw.window(800,800);
+        Draw.setFill(false);
+        Draw.setColor(0,0,255);
+        Draw.polygon(100,400,600,100,400,600,200,100,700,400);
+
+    }
